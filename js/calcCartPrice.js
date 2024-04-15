da61aac472ded19d42cb9a443a272e589bfd6fbf
Matvey Baklykov <matvey.bakl@gmail.com>
2024-04-15 18:34:01 +0300

    first commit

---
+++ js/calcCartPrice.js
@@ -0,0 +1,36 @@
+function calcCartPrice() {
+    const cartItems = document.querySelectorAll('.cart-item');
+
+    const totalPriceEl = document.querySelector('.total-price');
+
+    const deliveryCost = document.querySelector('.delivery-cost');
+    const cartDelivery = document.querySelector('[data-cart-delivery]');
+
+    let priceTotal = 0;
+
+
+    cartItems.forEach(function (item){
+        const amountEl = item.querySelector('[data-counter]');
+        const priceEl = item.querySelector('.price__currency');
+
+        const currentPrice = parseInt(amountEl.innerText) * parseInt(priceEl.innerText);
+        priceTotal += currentPrice;
+    });
+
+    totalPriceEl.innerText = priceTotal;
+
+    if (priceTotal > 0) {
+        cartDelivery.classList.remove('none');
+    } else {
+        cartDelivery.classList.add('none');
+    }
+
+    if (priceTotal >= 600) {
+        deliveryCost.classList.add('free');
+        deliveryCost.innerText = 'бесплатно';
+    } else {
+        deliveryCost.classList.remove('free');
+        deliveryCost.innerText = '250 ₽';
+    }
+
+}
---
+++ js/counter-02.js
@@ -0,0 +1,34 @@
+window.addEventListener('click', function() {
+    let counter;
+
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+        const counerWrapper = event.target.closest('.counter-wrapper');
+       
+        counter = counerWrapper.querySelector('[data-counter]'); 
+    }
+
+ 
+    if (event.target.dataset.action === 'plus') {
+  
+       counter.innerText = ++counter.innerText;
+    }
+
+    if (event.target.dataset.action === 'minus') { 
+
+       if (parseInt(counter.innerText) > 1) {
+        counter.innerText = --counter.innerText; 
+        } else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerText) === 1) {
+            event.target.closest('.cart-item').remove();
+
+            toggleCartStatus();
+
+            calcCartPrice();
+        }
+    }
+
+    if (event.target.hasAttribute('data-action') && event.target.closest('.cart-wrapper')) {
+        calcCartPrice();
+    }
+
+
+});
